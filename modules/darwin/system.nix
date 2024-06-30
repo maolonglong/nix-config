669c2ada2be27881784267044d048759e2c97424
Shaolong Chen <shaolong.chen@outlook.it>
2024-06-30 16:00:58 +0800

    Customize finder

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -47,14 +47,14 @@
       # };
 
       # customize finder
-      # finder = {
-      #   _FXShowPosixPathInTitle = true; # show full path in finder title
-      #   AppleShowAllExtensions = true; # show all file extensions
-      #   FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
-      #   QuitMenuItem = true; # enable quit menu item
-      #   ShowPathbar = true; # show path bar
-      #   ShowStatusBar = true; # show status bar
-      # };
+      finder = {
+        # _FXShowPosixPathInTitle = true; # show full path in finder title
+        AppleShowAllExtensions = true; # show all file extensions
+        FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
+        # QuitMenuItem = true; # enable quit menu item
+        ShowPathbar = true; # show path bar
+        # ShowStatusBar = true; # show status bar
+      };
 
       # customize trackpad
       trackpad = {
