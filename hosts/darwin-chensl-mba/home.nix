f5d2cc093d7298a6cb8959662a375a1a42ec3221
Shaolong Chen <shaolong.chen@outlook.it>
2024-07-04 17:33:29 +0800

    Split golang configs

--- hosts/darwin-chensl-mba/home.nix
+++ hosts/darwin-chensl-mba/home.nix
@@ -1,4 +1,4 @@
-{
+{lib, ...}: {
   programs.ssh = {
     enable = true;
     extraConfig = ''
@@ -10,5 +10,29 @@
       Host git-ssh.chensl.me
           ProxyCommand cloudflared access ssh --hostname %h
     '';
+  };
+
+  programs.go = rec {
+    enable = true;
+    goPath = "go";
+    goBin = "${goPath}/bin";
+    goPrivate = [
+      "github.com/maolonglong"
+      "go.chensl.me"
+    ];
+  };
+
+  home.sessionPath = [
+    "$GOPATH/bin"
+  ];
+
+  home.sessionVariables = {
+    GO111MODULE = "on";
+    GOPROXY = lib.concatStringsSep "|" [
+      "https://goproxy.cn"
+      "https://goproxy.io"
+      "https://proxy.golang.org"
+      "direct"
+    ];
   };
 }
--- hosts/darwin-work/home.nix
+++ hosts/darwin-work/home.nix
@@ -1,4 +1,29 @@
 {
   programs.ssh.enable = false;
   programs.git.enable = false;
+
+  # Configure GOPRIVATE and GOPROXY manually
+  programs.go = rec {
+    enable = true;
+    goPath = "go";
+    goBin = "${goPath}/bin";
+    # goPrivate = [
+    #   "github.com/maolonglong"
+    #   "go.chensl.me"
+    # ];
+  };
+
+  home.sessionPath = [
+    "$GOPATH/bin"
+  ];
+
+  home.sessionVariables = {
+    GO111MODULE = "on";
+    # GOPROXY = lib.concatStringsSep "|" [
+    #   "https://goproxy.cn"
+    #   "https://goproxy.io"
+    #   "https://proxy.golang.org"
+    #   "direct"
+    # ];
+  };
 }
