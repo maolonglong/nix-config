2f39abf8e3281871b76638ca5ad1730f88cd0a9f
Shaolong Chen <shaolong.chen@outlook.it>
2025-01-27 15:50:48 +0800

    fix: nix-darwin stable branch

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nix-darwin = {
-      url = "github:lnl7/nix-darwin";
+      url = "github:lnl7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
 
@@ -19,6 +19,7 @@
       # url = "github:ryan4yin/ragenix";
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.darwin.follows = "nix-darwin";
     };
 
     # add git hooks to format nix code before commit
