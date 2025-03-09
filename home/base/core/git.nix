b6de88f92d46eb931aa9133126febafdbbb41130
Shaolong Chen <shaolong.chen@outlook.it>
2025-03-09 22:12:04 +0800

    Update git config

--- home/base/core/git.nix
+++ home/base/core/git.nix
@@ -30,28 +30,63 @@ in {
     };
 
     extraConfig = {
-      core = {
-        editor = "vim";
-        # 也许不需要？
-        # sshCommand = "/usr/bin/ssh";
+      branch = {
+        sort = "-committerdate";
       };
       color = {
         ui = true;
       };
-      push = {
-        default = "simple";
+      column = {
+        ui = "auto";
       };
-      pull = {
-        rebase = true;
+      core = {
+        editor = "vim";
+
+        # 单独给大型仓库开
+        # git config core.fsmonitor true
+        # git config core.untrackedCache true
+        # fsmonitor = true;
+        # untrackedCache = true;
+
+        # 也许不需要？
+        # sshCommand = "/usr/bin/ssh";
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      help = {
+        autocorrect = "prompt";
       };
       init = {
         defaultBranch = "main";
       };
-      fetch = {
-        prune = true;
-      };
       log = {
         date = "format-local:%Y-%m-%d %H:%M:%S";
+      };
+      merge = {
+        conflictstyle = "zdiff3";
+      };
+      pull = {
+        rebase = true;
+      };
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+      tag = {
+        sort = "version:refname";
       };
     };
 
