fd2dd5c360e989fee8d0c15437c80a78029cfc9f
Shaolong Chen <shaolong.chen@outlook.it>
2025-09-04 09:40:56 +0800

    feat: update macOS finder and system settings to enhance user experience

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -43,12 +43,12 @@
 
       # customize finder
       finder = {
-        # _FXShowPosixPathInTitle = true; # show full path in finder title
+        _FXShowPosixPathInTitle = true; # show full path in finder title
         AppleShowAllExtensions = true; # show all file extensions
         FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
-        # QuitMenuItem = true; # enable quit menu item
+        QuitMenuItem = true; # enable quit menu item
         ShowPathbar = true; # show path bar
-        # ShowStatusBar = true; # show status bar
+        ShowStatusBar = true; # show status bar
       };
 
       # customize trackpad
@@ -61,36 +61,51 @@
 
       # customize macOS
       NSGlobalDomain = {
-        # defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false              # For VS Code
-        # defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false      # For VS Code Insider
-        # defaults write com.vscodium ApplePressAndHoldEnabled -bool false                      # For VS Codium
-        # defaults write com.microsoft.VSCodeExploration ApplePressAndHoldEnabled -bool false   # For VS Codium Exploration users
-        # defaults delete -g ApplePressAndHoldEnabled                                           # If necessary, reset global default
+        # `defaults read NSGlobalDomain "xxx"`
+        "com.apple.swipescrolldirection" = true; # enable natural scrolling(default to true)
+        "com.apple.sound.beep.feedback" = 0; # disable beep sound when pressing volume up/down key
+
+        # Appearance
+        # AppleInterfaceStyle = "Dark"; # dark mode
+
+        # AppleKeyboardUIMode = 3; # Mode 3 enables full keyboard control.
         # ApplePressAndHoldEnabled = true; # enable press and hold
 
-        InitialKeyRepeat = 15;
-        KeyRepeat = 2;
+        # If you press and hold certain keyboard keys when in a text area, the key’s character begins to repeat.
+        # This is very useful for vim users, they use `hjkl` to move cursor.
+        # sets how long it takes before it starts repeating.
+        InitialKeyRepeat = 15; # normal minimum is 15 (225 ms), maximum is 120 (1800 ms)
+        # sets how fast it repeats once it starts.
+        KeyRepeat = 2; # normal minimum is 2 (30 ms), maximum is 120 (1800 ms)
+
+        NSAutomaticCapitalizationEnabled = false; # disable auto capitalization(自动大写)
+        NSAutomaticDashSubstitutionEnabled = false; # disable auto dash substitution(智能破折号替换)
+        NSAutomaticPeriodSubstitutionEnabled = false; # disable auto period substitution(智能句号替换)
+        NSAutomaticQuoteSubstitutionEnabled = false; # disable auto quote substitution(智能引号替换)
+        NSAutomaticSpellingCorrectionEnabled = false; # disable auto spelling correction(自动拼写检查)
+        NSNavPanelExpandedStateForSaveMode = true; # expand save panel by default(保存文件时的路径选择/文件名输入页)
+        NSNavPanelExpandedStateForSaveMode2 = true;
       };
 
       # customize settings that not supported by nix-darwin directly
       # Incomplete list of macOS `defaults` commands :
       #   https://github.com/yannbertrand/macos-defaults
       CustomUserPreferences = {
-        # ".GlobalPreferences" = {
-        #   # automatically switch to a new space when switching to the application
-        #   AppleSpacesSwitchOnActivate = true;
-        # };
-        # NSGlobalDomain = {
-        #   # Add a context menu item for showing the Web Inspector in web views
-        #   WebKitDeveloperExtras = true;
-        # };
+        ".GlobalPreferences" = {
+          # automatically switch to a new space when switching to the application
+          AppleSpacesSwitchOnActivate = true;
+        };
+        NSGlobalDomain = {
+          # Add a context menu item for showing the Web Inspector in web views
+          WebKitDeveloperExtras = true;
+        };
         "com.apple.finder" = {
-          # AppleShowAllFiles = true;
-          # ShowExternalHardDrivesOnDesktop = true;
-          # ShowHardDrivesOnDesktop = true;
-          # ShowMountedServersOnDesktop = true;
-          # ShowRemovableMediaOnDesktop = true;
-          # _FXSortFoldersFirst = true;
+          AppleShowAllFiles = true;
+          ShowExternalHardDrivesOnDesktop = true;
+          ShowHardDrivesOnDesktop = true;
+          ShowMountedServersOnDesktop = true;
+          ShowRemovableMediaOnDesktop = true;
+          _FXSortFoldersFirst = true;
           # When performing a search, search the current folder by default
           FXDefaultSearchScope = "SCcf";
         };
@@ -99,36 +114,75 @@
           DSDontWriteNetworkStores = true;
           DSDontWriteUSBStores = true;
         };
-        # "com.apple.spaces" = {
-        #   "spans-displays" = 0; # Display have separate spaces
-        # };
-        # "com.apple.WindowManager" = {
-        #   EnableStandardClickToShowDesktop = 0; # Click wallpaper to reveal desktop
-        #   StandardHideDesktopIcons = 0; # Show items on desktop
-        #   HideDesktop = 0; # Do not hide items on desktop & stage manager
-        #   StageManagerHideWidgets = 0;
-        #   StandardHideWidgets = 0;
-        # };
-        # "com.apple.screensaver" = {
-        #   # Require password immediately after sleep or screen saver begins
-        #   askForPassword = 1;
-        #   askForPasswordDelay = 0;
-        # };
-        # "com.apple.screencapture" = {
-        #   location = "~/Desktop";
-        #   type = "png";
-        # };
-        # "com.apple.AdLib" = {
-        #   allowApplePersonalizedAdvertising = false;
-        # };
+        "com.apple.spaces" = {
+          # Display have separate spaces
+          #   true => disable this feature
+          #   false => enable this feature
+          "spans-displays" = true;
+        };
+        "com.apple.WindowManager" = {
+          EnableStandardClickToShowDesktop = 0; # Click wallpaper to reveal desktop
+          StandardHideDesktopIcons = 0; # Show items on desktop
+          HideDesktop = 0; # Do not hide items on desktop & stage manager
+          StageManagerHideWidgets = 0;
+          StandardHideWidgets = 0;
+        };
+        "com.apple.screensaver" = {
+          # Require password immediately after sleep or screen saver begins
+          askForPassword = 1;
+          askForPasswordDelay = 0;
+        };
+        "com.apple.screencapture" = {
+          location = "~/Desktop";
+          type = "png";
+        };
+        "com.apple.AdLib" = {
+          allowApplePersonalizedAdvertising = false;
+        };
         # Prevent Photos from opening automatically when devices are plugged in
-        # "com.apple.ImageCapture".disableHotPlug = true;
+        "com.apple.ImageCapture".disableHotPlug = true;
 
         "com.microsoft.VSCode".ApplePressAndHoldEnabled = false;
         "com.microsoft.VSCodeInsiders".ApplePressAndHoldEnabled = false;
         "com.vscodium".ApplePressAndHoldEnabled = false;
         "com.microsoft.VSCodeExploration".ApplePressAndHoldEnabled = false;
       };
+
+      loginwindow = {
+        GuestEnabled = false; # disable guest user
+        SHOWFULLNAME = true; # show full name in login window
+      };
+    };
+
+    # keyboard settings is not very useful on macOS
+    # the most important thing is to remap option key to alt key globally,
+    # but it's not supported by macOS yet.
+    keyboard = {
+      enableKeyMapping = false; # enable key mapping so that we can use `option` as `control`
+
+      # NOTE: do NOT support remap capslock to both control and escape at the same time
+      remapCapsLockToControl = false; # remap caps lock to control, useful for emac users
+      remapCapsLockToEscape = false; # remap caps lock to escape, useful for vim users
+
+      # swap left command and left alt
+      # so it matches common keyboard layout: `ctrl | command | alt`
+      #
+      # disabled, caused only problems!
+      swapLeftCommandAndLeftAlt = false;
+
+      userKeyMapping = [
+        # remap escape to caps lock
+        # so we swap caps lock and escape, then we can use caps lock as escape
+        # {
+        #   HIDKeyboardModifierMappingSrc = 30064771113;
+        #   HIDKeyboardModifierMappingDst = 30064771129;
+        # }
+      ];
     };
   };
+
+  # Set your time zone.
+  # comment this due to the issue:
+  #   https://github.com/LnL7/nix-darwin/issues/359
+  # time.timeZone = "Asia/shanghai";
 }
