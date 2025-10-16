3fade11bf126325c1dca460baacacd40562441ba
Shaolong Chen <shaolong.chen@outlook.it>
2025-10-16 10:07:12 +0800

    feat(starship): disable docker_context module

--- home/base/core/starship/default.nix
+++ home/base/core/starship/default.nix
@@ -11,6 +11,7 @@ in {
     settings =
       {
         container.disabled = true;
+        docker_context.disabled = true;
         palette = "catppuccin_mocha";
       }
       // builtins.fromTOML (builtins.readFile "${mynur.legacyPackages.${pkgs.system}.catppuccinThemes.starship}/themes/mocha.toml");
