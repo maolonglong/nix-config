41457c7a7038f1294d6ea4c0ca7e9da6f1a76648
Shaolong Chen <shaolong.chen@outlook.it>
2026-07-11 09:37:58 -0400

    perf(nix): add Catppuccin binary cache

--- modules/darwin/nix.nix
+++ modules/darwin/nix.nix
@@ -19,11 +19,13 @@
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
 
         "https://nix-community.cachix.org"
+        "https://catppuccin.cachix.org"
         # my own cache server, currently not used.
         # "https://maolonglong.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
         "maolonglong.cachix.org-1:da0YR8cbEEEtWhqWhz1AF5S6Awl+is2PV6Y34Si7Ivg="
       ];
       builders-use-substitutes = true;
